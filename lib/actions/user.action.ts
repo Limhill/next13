673e45871f23fe6ccaf3e6e3f2b50c200f996f14
Alibek Zhulmurzin <zhulmurzin@vtb.ru>
2024-03-26 12:19:37 +0100

    feat: add webhooks logic

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,6 +2,13 @@
 
 import { connectToDatabase } from "@/lib/mongoose";
 import { UserModel } from "@/database/user.model";
+import {
+  CreateUserParams,
+  DeleteUserParams,
+  UpdateUserParams,
+} from "@/lib/actions/shared.types";
+import { revalidatePath } from "next/cache";
+import { QuestionModel } from "@/database/question.model";
 
 export async function getUserById(params: { userId: string }) {
   try {
@@ -15,6 +22,61 @@ export async function getUserById(params: { userId: string }) {
   } catch (e) {
     console.log(e);
 
+    throw e;
+  }
+}
+
+export async function createUser(userData: CreateUserParams) {
+  try {
+    connectToDatabase();
+
+    const newUser = await UserModel.create(userData);
+
+    return newUser;
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+}
+
+export async function updateUser(params: UpdateUserParams) {
+  try {
+    connectToDatabase();
+
+    const { clerkId, updateData, path } = params;
+
+    await UserModel.findOneAndUpdate({ clerkId }, updateData, { new: true });
+
+    revalidatePath(path);
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+}
+
+export async function deleteUser(params: DeleteUserParams) {
+  try {
+    connectToDatabase();
+
+    const { clerkId } = params;
+
+    const user = await UserModel.findOneAndDelete({ clerkId });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    const userQuestionIds = await QuestionModel.find({
+      author: user._id,
+    }).distinct("_id");
+
+    await QuestionModel.deleteMany({ author: user._id });
+
+    const deletedUser = await UserModel.findOneAndDelete({ clerkId });
+
+    return deletedUser;
+  } catch (e) {
+    console.log(e);
     throw e;
   }
 }
