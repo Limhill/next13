a2d15c6d28b8f1475cef72a70cac24284cfbe3d5
Alibek Zhulmurzin <zhulmurzin@vtb.ru>
2024-03-27 05:19:45 +0100

    feat: add api webhook to public route

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -5,7 +5,7 @@ let isConnected: boolean = false;
 export const connectToDatabase = async () => {
   mongoose.set("strictQuery", true);
 
-  if (!process.env.MONGODB_URL) return console.log("MOSSING MONGODB_URL");
+  if (!process.env.MONGODB_URL) return console.log("MISSING MONGODB_URL");
 
   if (isConnected) {
     return console.log("MongoDB is already connected");
