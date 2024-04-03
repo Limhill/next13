4fa15883adda692abbcf4cc330c54cfd6fb92f41
Alibek Zhulmurzin <zhulmurzin@vtb.ru>
2024-04-03 08:50:00 +0200

    feat: create answer server action

---
+++ lib/actions/answer.action.ts
@@ -0,0 +1,29 @@
+"use server";
+
+import { CreateAnswerParams } from "@/lib/actions/shared.types";
+import { connectToDatabase } from "@/lib/mongoose";
+import { Answer } from "@/database/answer.model";
+import { Question } from "@/database/question.model";
+import { revalidatePath } from "next/cache";
+
+export async function createAnswer(params: CreateAnswerParams) {
+  try {
+    connectToDatabase();
+
+    const { content, author, question, path } = params;
+
+    const newAnswer = new Answer({ content, author, question });
+
+    // Add the answer to the question's answers array
+    await Question.findByIdAndUpdate(question, {
+      $push: { answers: newAnswer._id },
+    });
+
+    // TODO: Add interaction...
+
+    revalidatePath(path);
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+}
